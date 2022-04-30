6e1e4a1e4ba2252deea2e11e8335675681578e7b
Yuze Fu <i@xfox.me>
2022-04-30 21:13:28 +0800

    chore: unify encoding and line feed

--- src/Controllers/AnimeController.cs
+++ src/Controllers/AnimeController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,7 +15,7 @@ namespace Me.Xfox.ZhuiAnime.Controllers
                 ("名侦探柯南", "https://mikanani.me/images/Bangumi/201310/91d95f43.jpg"),
                 ("海贼王", "https://mikanani.me/images/Bangumi/201310/0aa598c7.jpg"),
                 ("火影忍者 博人传之火影次世代", "https://mikanani.me/images/Bangumi/201704/e46ad033.jpg"),
-                ("宝可梦 旅途‎", "https://mikanani.me/images/Bangumi/201911/0b25e1cd.jpg"),
+                ("宝可梦 旅途", "https://mikanani.me/images/Bangumi/201911/0b25e1cd.jpg"),
                 ("数码兽大冒险：", "https://mikanani.me/images/Bangumi/202004/2e66e770.jpg"),
                 ("王者天下 第三季", "https://mikanani.me/images/Bangumi/202004/5988c4af.jpg"),
                 ("舞伎家的料理人", "https://mikanani.me/images/Bangumi/202102/d74196db.jpg"),
