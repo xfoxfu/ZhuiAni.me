25f5e3c865b9c98c51e373cb721204a7d4c8b4b0
Yuze Fu <i@xfox.me>
2022-11-04 18:58:20 +0800

    feat: move episode api endpoint

--- Me.Xfox.ZhuiAnime/Controllers/EpisodeController.cs
+++ Me.Xfox.ZhuiAnime/Controllers/EpisodeController.cs
@@ -1,50 +1,23 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Me.Xfox.ZhuiAnime.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Me.Xfox.ZhuiAnime.Controllers;
 
 /// <summary>
 /// Get anime.
 /// </summary>
-[ApiController, Route("api/animes/{anime}/episodes")]
+[ApiController, Route("api/episodes")]
 public class EpisodeController : ControllerBase
 {
-    private ZAContext DbContext { get; init; }
-
-    public EpisodeController(ZAContext dbContext)
-    {
-        DbContext = dbContext;
-    }
-
     public record EpisodeDto(
         uint Id,
         string Name,
         string Title
     );
 
-    [HttpGet]
-    public async Task<IEnumerable<EpisodeDto>> GetEpisodesAsync(Anime anime)
-    {
-        return await DbContext.Entry(anime)
-            .Collection(a => a.Episodes!)
-            .Query()
-            .OrderBy(e => e.Name)
-            .Select(e => new EpisodeDto(e.Id, e.Name, e.Title))
-            .ToListAsync();
-    }
-
     [HttpGet("{episode}")]
-    public EpisodeDto GetEpisode(Anime anime, Episode episode)
+    public EpisodeDto GetEpisode(Episode episode)
     {
-        if (anime.Id != episode.AnimeId)
-        {
-            throw new ZhuiAnimeError.EpisodeNotBelongToAnime(episode.Id, anime.Id);
-        }
-
         return new EpisodeDto(
             episode.Id,
             episode.Name,
