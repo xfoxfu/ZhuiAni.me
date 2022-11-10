70c8c6868f9fb346030e1789b3d2e1a3510444dd
Yuze Fu <i@xfox.me>
2022-11-10 23:15:37 +0800

    build(webui): split vendor

--- Me.Xfox.ZhuiAnime.WebUI/vite.config.ts
+++ Me.Xfox.ZhuiAnime.WebUI/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), splitVendorChunkPlugin()],
   server: {
     proxy: {
       "/api": {
