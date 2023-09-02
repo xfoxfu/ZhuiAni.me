00f210c5e13ad4700644d030d6aaf6336c212f5f
Yuze Fu <i@xfox.me>
2023-09-02 16:22:18 +0900

    build: simplify vite and eslint
    - remove eslint and tsc from vite building
    - remove prettier from eslint
    - run prettier check and tsc as part of linting

--- Me.Xfox.ZhuiAnime.WebUI/vite.config.ts
+++ Me.Xfox.ZhuiAnime.WebUI/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), splitVendorChunkPlugin()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     proxy: {
       "/api": {
