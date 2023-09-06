0a86acde03a1ffee7b830c70078fde1afb974292
Yuze Fu <i@xfox.me>
2023-09-06 12:51:10 +0900

    feat(webui): refresh bangumi

--- Me.Xfox.ZhuiAnime.WebUI/src/components/anime/ItemLinks.tsx
+++ Me.Xfox.ZhuiAnime.WebUI/src/components/anime/ItemLinks.tsx
@@ -1,11 +1,36 @@
 import api from "../../api";
-import { Alert, AlertDescription, AlertIcon, HStack, Icon, Link, Progress, Tag, Text, Tooltip } from "@chakra-ui/react";
+import { promiseWithToast } from "../../utils";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  HStack,
+  Icon,
+  IconButton,
+  Link,
+  Progress,
+  Tag,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import React from "react";
-import { IoLinkOutline, IoOpenOutline, IoVideocamOutline } from "react-icons/io5";
+import { IoLinkOutline, IoOpenOutline, IoRefresh, IoVideocamOutline } from "react-icons/io5";
 import MIMEType from "whatwg-mimetype";
 
 export const ItemLinks: React.FunctionComponent<{ id: number }> = ({ id }) => {
   const { data: links, error } = api.useItemLinkList(id);
+  const bangumi =
+    links &&
+    links.find((x) => x.address.startsWith("https://bgm.tv/subject/") && x.mime_type === "text/html;kind=bgm.tv");
+  const onRefresh = () =>
+    promiseWithToast(async () => {
+      if (!bangumi) {
+        return;
+      }
+      await api.bangumiImportSubject({
+        id: Number.parseInt(bangumi.address.replace("https://bgm.tv/subject/", ""), 10),
+      });
+    });
 
   return (
     <>
@@ -37,6 +62,11 @@ export const ItemLinks: React.FunctionComponent<{ id: number }> = ({ id }) => {
               </Tooltip>
             </Link>
           ))}
+        {bangumi && (
+          <Tooltip label="Refresh bgm.tv">
+            <IconButton aria-label="refresh" colorScheme="teal" as={IoRefresh} onClick={onRefresh} />
+          </Tooltip>
+        )}
       </HStack>
     </>
   );
--- Me.Xfox.ZhuiAnime.WebUI/src/pages/ItemsList.tsx
+++ Me.Xfox.ZhuiAnime.WebUI/src/pages/ItemsList.tsx
@@ -40,7 +40,7 @@ export const ItemsList: React.FunctionComponent = () => {
           {/* <ImportBangumi /> */}
         </HStack>
         <Box>
-          <SimpleGrid spacingX="3" spacingY="2" alignItems="stretch" minChildWidth="16">
+          <SimpleGrid spacingX="3" spacingY="2" alignItems="stretch" minChildWidth="24ch">
             {animes?.map((a) => (
               <GridItem key={a.id} px="3" py="2" borderWidth="1px" rounded="md" bg="white">
                 <VStack height="100%" maxWidth="32ch" justifyContent="space-between">
